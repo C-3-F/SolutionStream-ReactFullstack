224118502eecb91ee741efdca2f91f0b8f278474
Cannon Farr <cfarr@SolutionStream.com>
2018-07-10 15:57:40 -0600

    made landing and oauth front end

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/current_user').then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+//helps send object to front-end to tell if user is signed in
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
