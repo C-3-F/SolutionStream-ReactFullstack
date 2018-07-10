85e4116011cf936a7be5a2ab3787066a663321c2
Cannon Farr <cfarr@SolutionStream.com>
2018-07-10 17:30:11 -0600

    front-end work

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,7 @@ module.exports = {
   googleClientID: '1018496235627-mfrmbloii108ggn6eamp3knee2ecgi14.apps.googleusercontent.com',
   googleClientSecret: 'zlnoUHAsXn4Y0qvhQ1accd8Y',
   mongoURI: 'mongodb://c.3.f:password1@ds231961.mlab.com:31961/emaily-dev',
-  cookieKey: 'aqrweouilkjokhasdflkjasdfkjhasdf'
+  cookieKey: 'aqrweouilkjokhasdflkjasdfkjhasdf',
+  stripePublishableKey: 'pk_test_wVYNWRllMgp7N711GZOmtXZW',
+  stripeSecretKey: 'sk_test_Bw2yNDMZKmKvcw1we4iOv86T'
 };
