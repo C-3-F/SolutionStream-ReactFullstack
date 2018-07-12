b0cad671273f0422c9de07cc84f19f9f17e35b50
Cannon Farr <cfarr@SolutionStream.com>
2018-07-12 14:57:04 -0600

    set up stripe

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -22,6 +25,7 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 app.listen(port, host, function() {
   console.log('Server started.......');
