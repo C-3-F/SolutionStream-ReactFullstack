8b2f5352b3472cbc25167737021246a41975c2ff
Cannon Farr <cfarr@SolutionStream.com>
2018-09-26 12:39:10 -0600

    Finished React FUllstack

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,15 +9,19 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const _ = require('lodash');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your feedback');
+  });
+
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+    res.send(surveys);
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
     console.log(req.body);
     const p = new Path('/api/surveys/:surveyId/:choice');
-    const events = _
-      .chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -26,10 +30,22 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(async ({ surveyId, email, choice }) => {
+        await Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
       .value();
-
-    //console.log(events);
-    //console.log('through');
 
     res.send({});
   });
